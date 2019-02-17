7f3a5f3f6c6fed38d814975b865f3cfcec7bfa52
mcebisi <mcebisimkhohliwe@gmail.com>
2019-02-17 12:56:18 +0200

    Implemented horizontals

---
+++ Magic_Square/Services/Horizontals.cs
@@ -0,0 +1,36 @@
+﻿using Magic_Square.Interfaces;
+using System;
+using System.Linq;
+
+namespace Magic_Square.Services
+{
+    public class Horizontals:IMagic_Square
+    {
+        private IMagic_Square _nextDirection;
+        public void SetSuccessor(IMagic_Square nextDirection)
+        {
+            _nextDirection = nextDirection;
+        }
+
+        public bool IsMagicSquare(int[][] square,int sum)
+        {
+            var validHorizontals = IsValid(square, sum);
+
+            if (validHorizontals && HasNextDirection())
+            {
+                _nextDirection.IsMagicSquare(square, sum);
+            }
+            return validHorizontals;
+        }
+
+        private bool IsValid(int[][] square,int sum)
+        {
+            return square.All(line => line.Sum() == sum);
+        }
+
+        private bool HasNextDirection()
+        {
+            return _nextDirection != null;
+        }
+    }
+}
