badfe43ba00d85e527cc4d8f11c0fdc6511bc1a2
mcebisi <mcebisimkhohliwe@gmail.com>
2019-02-17 12:53:30 +0200

    Created interface for setting up next direction

---
+++ Magic_Square/Interfaces/IMagic_Square.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic_Square.Interfaces
+{
+    public interface IMagic_Square
+    {
+        void SetSuccessor(IMagic_Square nextDirection);
+        bool IsMagicSquare(int[][] square,int sum);
+    }
+}
