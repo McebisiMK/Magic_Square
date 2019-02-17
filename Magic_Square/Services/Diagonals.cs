8b2674ebfce8484c89978c49a8fff7c8094e32ce
mcebisi <mcebisimkhohliwe@gmail.com>
2019-02-17 13:03:48 +0200

    Tested that the implementation accommodates different sizes of squares

---
+++ Magic_Square/Services/Diagonals.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Magic_Square.Interfaces;
+
+namespace Magic_Square.Services
+{
+    public class Diagonals:IMagic_Square
+    {
+        private IMagic_Square _nextDirection;
+        public void SetSuccessor(IMagic_Square nextDirection)
+        {
+            _nextDirection = nextDirection;
+        }
+
+        public bool IsMagicSquare(int[][] square, int sum)
+        {
+            var diagonals = GetDiagonals(square);
+            var validDiagonals = IsValid(diagonals, sum);
+
+            if (validDiagonals && HasNextDirection())
+            {
+                _nextDirection.IsMagicSquare(square, sum);
+            }
+            return validDiagonals;
+        }
+
+        private int [][] GetDiagonals(int[][] square)
+        {
+            var length = square.Length;
+            var topLeftToBottomRight = Enumerable.Range(0, length)
+                                            .Select(index => square[index][index]).ToArray();
+
+            var bottomLeftToTopRight = Enumerable.Range(0, length)
+                                            .Select(index => square[length - index - 1][index]).ToArray();
+
+            var diagonals = new[] { topLeftToBottomRight, bottomLeftToTopRight };
+
+            return diagonals;
+        }
+
+        private bool IsValid(int[][] diagonals, int sum)
+        {
+            return diagonals.All(line => line.Sum() == sum);
+        }
+
+        private bool HasNextDirection()
+        {
+            return _nextDirection != null;
+        }
+    }
+}
---
+++ Magic_Square/Tests/SquareTests.cs
@@ -0,0 +1,120 @@
+﻿using Magic_Square.Interfaces;
+using Magic_Square.Services;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Magic_Square.Tests
+{
+    [TestFixture]
+    class SquareTests
+    {
+        [Test]
+        public void IsMagicSqaure_GivenMagicSquare_ShouldBeAbleToStartByHorizontalsAndMoveToVerticalsThenDiagonals()
+        {
+            //----------------------Arrange---------------------------
+            var sum = 15;
+            var square = new[] {new[] { 2, 7, 6 },
+                                new[] { 9, 5, 1 },
+                                new[] { 4, 3, 8 } };
+            var horizontals = CreateHorizontals();
+            var verticals = CreateVerticals();
+            var diagonals = Substitute.For<IMagic_Square>();
+
+            horizontals.SetSuccessor(verticals);
+            verticals.SetSuccessor(diagonals);
+
+            //----------------------Act-------------------------------
+            horizontals.IsMagicSquare(square, sum);
+
+            //----------------------Assert----------------------------
+            diagonals.Received(1).IsMagicSquare(square, sum);
+
+        }
+
+        [Test]
+        public void IsMagicSqaure_GivenMagicSquare_ShouldBeAbleToStartByVerticalsAndMoveToDiagonalsThenHorizontals()
+        {
+            //----------------------Arrange---------------------------
+            var sum = 15;
+            var square = new[] {new[] { 2, 7, 6 },
+                                new[] { 9, 5, 1 },
+                                new[] { 4, 3, 8 } };
+            var verticals = CreateVerticals();
+            var diagonals = CreateDiagonals();
+            var horizontals = Substitute.For<IMagic_Square>();
+
+            verticals.SetSuccessor(diagonals);
+            diagonals.SetSuccessor(horizontals);
+
+            //----------------------Act-------------------------------
+            verticals.IsMagicSquare(square, sum);
+
+            //----------------------Assert----------------------------
+            horizontals.Received(1).IsMagicSquare(square, sum);
+
+        }
+
+        [Test]
+        public void IsMagicSqaure_GivenMagicSquare_ShouldBeAbleToStartByDiagonalsAndMoveHorizontalsThenVerticals()
+        {
+            //----------------------Arrange---------------------------
+            var sum = 15;
+            var square = new[] {new[] { 2, 7, 6 },
+                                new[] { 9, 5, 1 },
+                                new[] { 4, 3, 8 } };
+            var diagonals = CreateDiagonals();
+            var horizontals = CreateHorizontals();
+            var verticals = Substitute.For<IMagic_Square>();
+
+            diagonals.SetSuccessor(horizontals);
+            horizontals.SetSuccessor(verticals);
+
+            //----------------------Act-------------------------------
+            diagonals.IsMagicSquare(square, sum);
+
+            //----------------------Assert----------------------------
+            verticals.Received(1).IsMagicSquare(square, sum);
+
+        }
+
+        [Test]
+        public void IsMagicSqaure_GivenFourByFourSidesMagicSquare_ShouldBeAbleToStartByDiagonalsAndMoveHorizontalsThenVerticals()
+        {
+            //----------------------Arrange---------------------------
+            var sum = 98;
+            var square = new[] {new[] { 5, 6, 19, 68 },
+                                new[] { 69, 18, 3, 8 },
+                                new[] { 4, 7, 70, 17},
+                                new []{20, 67, 6, 5} };
+
+            var diagonals = CreateDiagonals();
+            var horizontals = CreateHorizontals();
+            var verticals = Substitute.For<IMagic_Square>();
+
+            diagonals.SetSuccessor(horizontals);
+            horizontals.SetSuccessor(verticals);
+
+            //----------------------Act-------------------------------
+            diagonals.IsMagicSquare(square, sum);
+
+            //----------------------Assert----------------------------
+            verticals.Received(1).IsMagicSquare(square, sum);
+
+        }
+
+        private static Diagonals CreateDiagonals()
+        {
+            return new Diagonals();
+        }
+
+        private static Verticals CreateVerticals()
+        {
+            return new Verticals();
+        }
+
+        private static Horizontals CreateHorizontals()
+        {
+            return new Horizontals();
+        }
+    }
+}
